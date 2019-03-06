122ae0712ef7ffaff28dd7cffe6619351a8a272b
sebastien-p <sp@fing.pro>
2019-03-06 18:16:39 +0100

    Prevent collapsing issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,5 +4,6 @@ export const Header = styled.header`
   align-items: center;
   background: ${({ theme }) => theme.twitchColor};
   display: flex;
+  flex: none;
   justify-content: space-between;
 `;
