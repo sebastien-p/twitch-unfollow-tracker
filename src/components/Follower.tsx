9a2bd9174333d092c84153fcebf42abbd6cb93cb
sebastien-p <sp@fing.pro>
2019-03-06 18:16:28 +0100

    Add label, WIP

--- src/components/Follower.tsx
+++ src/components/Follower.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, Fragment } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { Follower as FollowerModel } from '../services/database';
 
@@ -8,9 +9,16 @@ export type FollowerProps = {
 
 export const Follower: FunctionComponent<FollowerProps> = (
   { data: { name, date } }
-) => (
-  <Fragment>
-    <a href={`https://twitch.tv/${name}`}>{name}</a>
-    <time dateTime={date}>{date}</time>
-  </Fragment>
-);
+) => {
+  const [t] = useTranslation();
+
+  return (
+    <Fragment>
+      <a href={`https://twitch.tv/${name}`}>{name}</a>
+      <p>
+        {t('followDate')}
+        <time dateTime={date}>{date}</time>
+      </p>
+    </Fragment>
+  );
+};
