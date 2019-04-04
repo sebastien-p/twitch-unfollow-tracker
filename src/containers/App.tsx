3e2108897ee4ab4d6c4722b86ee14201be086fc7
sebastien-p <sp@fing.pro>
2019-04-04 18:39:41 +0200

    Use strict mode

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, StrictMode } from 'react';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -11,15 +11,17 @@ import { Login } from './Login';
 import { Home } from './Home';
 
 export const App: FunctionComponent = () => (
-  <Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <Styles>
-        <BrowserRouter>
-          <Routes path='/' component={Home}>
-            <Public path='/login' component={Login}/>
-          </Routes>
-        </BrowserRouter>
-      </Styles>
-    </PersistGate>
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Styles>
+          <BrowserRouter>
+            <Routes path='/' component={Home}>
+              <Public path='/login' component={Login}/>
+            </Routes>
+          </BrowserRouter>
+        </Styles>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
