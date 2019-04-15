236cc5a1f44297a6707026fb6b7ab2468be2aa4e
sebastien-p <sp@fing.pro>
2019-04-15 18:25:43 +0200

    Fix typing issue

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,8 +1,8 @@
-import { ComponentType } from 'react';
+import { FunctionComponent } from 'react';
 import { FieldConfig, Field } from 'formik';
 import styled from 'styled-components/macro';
 
-export const Input = styled(Field as ComponentType<FieldConfig>)`
+export const Input = styled(Field as FunctionComponent<FieldConfig>)`
   background: ${({ theme }) => theme.secondary2};
   border: 1px solid;
   border-radius: ${({ theme }) => theme.spacing / 2}px;
